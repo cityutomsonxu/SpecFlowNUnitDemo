65b8e96ebc8bbfbd13c44470e0c024068abf2912
CityU Tomson Xu <tomsonxu@um.cityu.edu.hk>
2015-01-13 11:18:15 +0800

    init version

---
+++ SpecFlowNUnitDemo/SpecFlowFeature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+namespace SpecFlowNUnitDemo
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
